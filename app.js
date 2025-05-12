16dd9fad59143771f31212e383e1bfc8437edadc
mathaes33 <mathaes33@gmail.com>
2025-05-12 09:52:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import HomeScreen from './screens/HomeScreen';
-import SnakeDetailScreen from './screens/SnakeDetailScreen';
-import BreedingLab from './screens/BreedingLab';
-
-const Stack = createNativeStackNavigator();
+import Navigation from './Navigation';
 
 export default function App() {
-  return (
+  return <Navigation />;
+}
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
